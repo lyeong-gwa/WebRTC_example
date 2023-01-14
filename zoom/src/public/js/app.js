aa9021063ecbe1ed2aa6d4fac653f91217a85442
박정희 <wjdgmll77@naver.com>
2023-01-14 14:39:44 +0900

    소켓 설정까지

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,1 +1,2 @@
-alert("hi");
+//alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`); //그냥 http://localhost하면 안됨
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,4 +1,5 @@
 import http from "http"; //웹소켓과 express합치기 별도의 ws서버를 구축하지 않는다.
+import WebSocket from "ws"; //ws 추가
 import express from "express"; //node서버 express사용
 const app = express();
 //console.log("hello");
@@ -8,13 +9,25 @@ const app = express();
 app.set("view engine", "pug"); //뷰 엔진 pug 명시
 app.set("views", __dirname + "/views"); // views폴더를 현재 디렉터리/views로
 app.use("/public", express.static(__dirname + "/public")); //public폴더역할 지정
-app.get("/", (req, res) => res.render("home")); //home.pug를 찾아간다. 루트 도메인
-app.get("/*", (req,res) => res.redirect("/")); // 이외 나머지들은 /로 리다이렉트시킨다.
+app.get("/", (_, res) => res.render("home")); //home.pug를 찾아간다. 루트 도메인
+app.get("/*", (_, res) => res.redirect("/")); // 이외 나머지들은 /로 리다이렉트시킨다.
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
+//일반적으로 http서버 구현
 //app.listen(3000, handleListen);
 
 //이런형식으로 하면 websocket과 http서버를 동시에 돌릴 수 있다.
-const server = http.createServer(app); 
-const wss = new WebSocket.Server({server});
-server.listen(3000,handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+
+//ws 통신 대기
+function handleConnection(socket) {
+    console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+
+
+server.listen(3000, handleListen);
