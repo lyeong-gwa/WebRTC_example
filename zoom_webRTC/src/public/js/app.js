1620370bbafc949667db45385dae273e27707153
박정희 <wjdgmll77@naver.com>
2023-01-15 19:17:42 +0900

    ice candidate까지 (1:1통신)

--- zoom_webRTC/src/public/js/app.js
+++ zoom_webRTC/src/public/js/app.js
@@ -4,20 +4,27 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
 
-
+call.hidden = true;
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection; //상호간의 연락을 위한
 
 async function getCameras() {
     try {
-        const devices = await navigator.mediaDevices.enumerateDevices(); //사용자의 미디어 디바이스 목록 가져온다.
+        const devices = await navigator.mediaDevices.enumerateDevices();//사용자의 미디어 디바이스 목록 가져온다.
         const cameras = devices.filter((device) => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
         cameras.forEach((camera) => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {//카메라 디바이스 중에서 선택한 값에 해당하는 디바이스를 활성화
+                option.selected = true;
+            }
             camerasSelect.appendChild(option);
         });
     } catch (e) {
@@ -25,21 +32,35 @@ async function getCameras() {
     }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+    const initialConstrains = { //전면카메라
+        audio: true,
+        video: { facingMode: "user" },
+    };
+    const cameraConstraints = {//후면카메라
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    };
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
+        // 카메라 예제에서 사용
+        // myStream = await navigator.mediaDevices.getUserMedia({
+        //     audio: true,
+        //     video: true,
+        // });
+
+        //
+
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+        );
         myFace.srcObject = myStream;
-        await getCameras();
+        if (!deviceId) {
+            await getCameras();
+        }
     } catch (e) {
         console.log(e);
     }
 }
-
-getMedia();
-
 function handleMuteClick() {
     myStream
         .getAudioTracks()
@@ -65,5 +86,84 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+//getMedia(); 이제 ui에서 불러오므로 생략
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+//-아래코드는 방과 관련한 코드--------------------------------------------
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() { //startMedia() -> initCall
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();//이제 여기서 미디어를 시작한다.
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    //socket.emit("join_room", input.value, startMedia); //answer실습위치에서 done삭제하였음
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", async (offer) => {
+    console.log("received the offer");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
+});
+
+socket.on("answer", (answer) => {
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
+  });
+
+// RTC Code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream
+        .getTracks()
+        .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+  }
+  
+  function handleAddStream(data) {
+    console.log("peer가 보낸 이벤트(스트림)-> 이걸로 나랑 연락할 수 있어",data);
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+  }
