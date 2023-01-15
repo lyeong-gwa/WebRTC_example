1729c1510db9085232d0b9fa69c677ad9770a5cb
박정희 <wjdgmll77@naver.com>
2023-01-15 11:34:53 +0900

    이해할 수 있도록 내용수정 및 방채팅까지

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,25 +1,96 @@
 //브라우저 역할
 //alert("hi");
-const socket = new WebSocket(`ws://${window.location.host}`); //그냥 http://localhost하면 안됨
-socket.addEventListener("open", () => {
-    console.log("연결 시작");
-})
-socket.addEventListener("message", (message) => {
-    console.log("메세지: ", message.data);
-});
-socket.addEventListener("close", () => {
-    console.log("연결 해제");
+//socket 영역
+// const socket = new WebSocket(`ws://${window.location.host}`); //그냥 http://localhost하면 안됨
+// socket.addEventListener("open", () => {
+//     console.log("연결 시작");
+// })
+// socket.addEventListener("message", (message) => {
+//     const li = document.createElement("li");
+//     li.innerText = message.data;
+//     messageList.append(li);
+// });
+// socket.addEventListener("close", () => {
+//     console.log("연결 해제");
+// });
+
+// //home.pug에 있는 요소들 관련
+// const messageList = document.querySelector("ul");
+
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+
+// function makeMessage(type, payload) {
+//     const msg = { type, payload };
+//     return JSON.stringify(msg);
+// }
+
+// //home의 submit 이벤트 캐치시 input의 데이터들을 socket으로 보내고 내용을 비운다.
+// messageForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const input = messageForm.querySelector("input");
+//     socket.send(makeMessage("new_message", input.value));
+//     input.value = "";
+// });
+
+// nickForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const input = nickForm.querySelector("input");
+//     socket.send(makeMessage("nickname", input.value));
+// });
+//
+//위 코드는 socket IO 안쓰고 구현--------------------------------------------------------------------------------------
+
+const socket = io();
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+let roomName; //자신이 들어가있는 방이름
+room.hidden = true; // 방에들어가면 방들어가는 것과 채팅창 변경
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("누군가 채팅에 들어왔습니다.");
 });
 
-
-//home.pug에 있는 요소 커넥
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
-//home의 submit 이벤트 캐치시 input의 데이터들을 socket으로 보내고 내용을 비운다.
-messageForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(input.value);
-    input.value = "";
+socket.on("bye", () => {
+  addMessage("누군가 채팅에서 나갔습니다.");
 });
+
+socket.on("new_message", addMessage);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,48 +1,70 @@
 import http from "http"; //웹소켓과 express합치기 별도의 ws서버를 구축하지 않는다.
-import WebSocket from "ws"; //ws 추가
+import { Server } from "socket.io"; // npm i socket.io
 import express from "express"; //node서버 express사용
 const app = express();
-//console.log("hello");
-
-//app.listen(3000);
 
 app.set("view engine", "pug"); //뷰 엔진 pug 명시
 app.set("views", __dirname + "/views"); // views폴더를 현재 디렉터리/views로
 app.use("/public", express.static(__dirname + "/public")); //public폴더역할 지정
 app.get("/", (_, res) => res.render("home")); //home.pug를 찾아간다. 루트 도메인
 app.get("/*", (_, res) => res.redirect("/")); // 이외 나머지들은 /로 리다이렉트시킨다.
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-//일반적으로 http서버 구현
-//app.listen(3000, handleListen);
-
-//이런형식으로 하면 websocket과 http서버를 동시에 돌릴 수 있다.
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
 
+//이런형식은 websocket과 http서버를 동시에 돌리는 방법
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.onAny((event) => { //모든 이벤트에 대해 listen
+        console.log(`Socket Event: ${event}`);
+    });
+
+
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName); //간단히 방에 등록가능
+        done();
+        socket.to(roomName).emit("welcome");
+    });
+
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    });
+
+
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
+});
+
+
+httpServer.listen(3000, () => { console.log(`Listening on http://localhost:3000`) });
 
 //ws 통신 대기
-// function handleConnection(socket) { //서버 소켓
-//     console.log(socket);
-// }
-// wss.on("connection", handleConnection);
-
 // wss.on("connection", (socket) =>{ //익명함수 선언방식
 //     console.log(socket);
 // });
 
-
-const sockets = []; //소켓들 전부 관리
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    console.log("새로운 연결");
-    socket.on("close", () => console.log("브라우저와 연결해제"));
-    socket.on("message", (message) => {
-      sockets.forEach((new_socket)=> new_socket.send(message.toString())); //노마드에서 그냥 메세지지만 버전오류로 toString하면 blob대신 메세지 전달됨
-    });
-    socket.send("서버와 연결하였습니다."); //브라우저에 전송하는 메세지
-  });
-
-
-server.listen(3000, handleListen);
+//이 코드는 socket IO 안쓰고 구현한 예제코드 방관리 등 기능구현 하나하나 해야됨
+//import WebSocket from "ws"; //ws 추가
+//const wss = new WebSocket.Server({ server });
+// const sockets = []; //소켓들 전부 관리
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "익명";
+//     console.log("새로운 연결");
+//     socket.on("close", () => console.log("브라우저와 연결해제"));
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {//메세지 타입에 따라 다른 처리
+//           case "new_message":
+//             sockets.forEach((aSocket) =>
+//               aSocket.send(`${socket.nickname}: ${message.payload}`)
+//             );
+//           case "nickname":
+//             socket["nickname"] = message.payload;
+//         }
+//     });
+//     socket.send("서버와 연결하였습니다."); //브라우저에 전송하는 메세지
+//   });
